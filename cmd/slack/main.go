75abad4936ac9a2f09efb2032aa52d5600bf93a4
nice-pink <r@nice.pink>
2025-02-27 15:19:21 +0100

    feat: move functionality to pkg.

--- cmd/slack/main.go
+++ cmd/slack/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"os"
-	"path"
 
 	"github.com/nice-pink/goutil/pkg/log"
-	"github.com/slack-go/slack"
+	"github.com/nice-pink/slack-app/pkg/msg"
 )
 
 func main() {
@@ -20,23 +18,23 @@ func main() {
 	fileTitle := flag.String("fileTitle", "", "File title.")
 	flag.Parse()
 
-	client := GetClient()
+	client := msg.GetClient()
 	if *header != "" || *text != "" {
-		err := SendMsg(*header, *text, *color, *channelId, client)
+		err := msg.SendMsg(*header, *text, *color, *channelId, client)
 		if err != nil {
 			os.Exit(2)
 		}
 	}
 
 	if *file != "" {
-		err := SendFile(*file, *fileTitle, *channelId, client)
+		err := msg.SendFile(*file, *fileTitle, *channelId, client)
 		if err != nil {
 			os.Exit(2)
 		}
 	}
 
 	if *folder != "" {
-		errs := SendFiles(*folder, *fileTitle, *channelId, client)
+		errs := msg.SendFiles(*folder, *fileTitle, *channelId, client)
 		if len(errs) > 0 {
 			os.Exit(2)
 		}
@@ -44,110 +42,4 @@ func main() {
 
 	log.Info("*** Start")
 	log.Info(os.Args)
-}
-
-func GetClient() *slack.Client {
-	token := os.Getenv("SLACK_TOKEN")
-	return slack.New(token)
-}
-
-func SendMsg(header, text, color, channelId string, client *slack.Client) error {
-	attachment := slack.Attachment{
-		Text: text,
-		// Fields: []slack.AttachmentField{
-		// 	slack.AttachmentField{
-		// 		Title: "title",
-		// 		Value: "value",
-		// 	},
-		// },
-	}
-	if color != "" {
-		attachment.Color = color
-	}
-
-	channel, ts, err := client.PostMessage(
-		channelId,
-		slack.MsgOptionText(header, false),
-		slack.MsgOptionAttachments(attachment),
-		slack.MsgOptionAsUser(true),
-	)
-	if err != nil {
-		log.Err(err, "post message")
-		return err
-	}
-	log.Info(ts, ":: Posted message to", channel)
-
-	return nil
-}
-
-func GetBlock(header, text string, client *slack.Client) ([]byte, error) {
-	var headerSection *slack.SectionBlock
-	if header != "" {
-		headerObj := slack.NewTextBlockObject("mrkdwn", header, false, false)
-		headerSection = slack.NewSectionBlock(headerObj, nil, nil)
-	}
-
-	var fieldsSection *slack.SectionBlock
-	if text != "" {
-		blockText := slack.NewTextBlockObject("mrkdwn", text, false, false)
-		fieldsSection = slack.NewSectionBlock(nil, []*slack.TextBlockObject{blockText}, nil)
-	}
-
-	msg := slack.NewBlockMessage(
-		headerSection,
-		fieldsSection,
-	)
-
-	body, err := json.MarshalIndent(msg, "", "    ")
-	if err != nil {
-		log.Err(err, "marshal message")
-		return nil, err
-	}
-
-	return body, nil
-}
-
-func SendFile(filepath, title, channel string, client *slack.Client) error {
-	log.Info("Send file", filepath)
-	filename := path.Base(filepath)
-
-	fileInfo, err := os.Stat(filepath)
-	if err != nil {
-		log.Err(err, "stat file", filepath)
-		return err
-	}
-
-	params := slack.UploadFileV2Parameters{
-		Title:    title,
-		File:     filepath,
-		FileSize: int(fileInfo.Size()),
-		Filename: filename,
-		Channel:  channel,
-	}
-	file, err := client.UploadFileV2(params)
-	if err != nil {
-		log.Err(err, "upload file error", filepath)
-		return err
-	}
-	log.Info("Uploaded", file.ID, file.Title)
-	return nil
-}
-
-func SendFiles(folder, title, channel string, client *slack.Client) []error {
-	files, err := os.ReadDir(folder)
-	if err != nil {
-		return []error{err}
-	}
-
-	errs := []error{}
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-		err := SendFile(path.Join(folder, file.Name()), file.Name(), channel, client)
-		if err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return errs
 }
